b177edb79d0cc7bff5460c6480ec18ecc4a9837f
Métodos Exatos <37481514+metodosexatos@users.noreply.github.com>
2019-08-09 09:01:36 -0300

    Add files via upload

---
+++ ECONI-Sec05_01_IC_Coeficientes.R
@@ -0,0 +1,96 @@
+############################
+####### Métodos Exatos ######
+### www.metodosexatos.com ###
+#############################
+
+# Autor: André Santos | andre@metodosexatos.com.br
+# 17/07/2019
+
+# To cite R in publications use:
+# citation()
+
+###################################################################
+
+#--------------------- Diretórios e Arquivos ---------------------#
+
+# getwd() # Qual o diretório que o script está apontando
+# list.files() # Quais arquivos estão contidos no diretório
+# setwd("C:/Users/andre/OneDrive/Documentos/PROJETOS/Metodos Exatos/Cursos/Curso017_Econometria_I/Curso-ECON_Material_apoio/Datasets_Econ-I")
+
+# Leitura de uma base externa
+# leitura_csv2 <- read.csv2(file = "exemplo.csv")
+
+# Exportação de um arquivo no formato csv2 (formato brasileiro):
+# write.csv2(frame_carros, "exemplo.csv")
+
+# Leitura de arquivo externo usando pacotes
+
+# if (!require(package)) install.packages("xlsx")
+# library(xlsx)
+
+# read.xlsx("exemplo.xlsx", sheetName = "nome_planilha")
+
+# Salvar uma arquivo no formato xlsx
+# write.xlsx(nome_dataframe, "exemplo.xlsx")
+
+#---------------------------------------------------------------#
+
+#************* Intervalo de Confiança para os Coeficientes *************#
+
+# Variáveis
+X <- c(80, 100, 120, 140, 160, 180, 200, 220, 240, 260)
+Y <- c(60, 85, 79, 115, 107, 115, 140, 135, 155, 191)
+
+# FRA
+
+FRA <- lm(Y~X)
+summary(FRA)
+
+# Coeficientes
+
+install.packages("broom") # extrair algumas informações do FRA
+library("broom")
+
+info_cf <- tidy(FRA)
+info_cf
+
+# Estimadores dos coeficientes
+beta1 <- info_cf$estimate[1]
+beta2 <- info_cf$estimate[2]
+beta1
+beta2
+
+# Erro padrão dos estimadores
+erro_beta1 <- info_cf$std.error[1]
+erro_beta2 <- info_cf$std.error[2]
+erro_beta1
+erro_beta2
+
+# Estatistica t
+
+t <- t.test(X, Y, "two.sided", conf.level = 0.95)
+t
+
+estatistica_t <- t$statistic[1]
+estatistica_t
+
+# Intervalo de Confiança (IC)
+
+#- IC beta 1
+li_beta1 <- beta1 - estatistica_t*erro_beta1
+li_beta1
+ls_beta1 <- beta1 + estatistica_t*erro_beta1
+ls_beta1
+
+#- IC beta 2
+li_beta2 <- beta2 - estatistica_t*erro_beta2
+li_beta2
+ls_beta2 <- beta2 + estatistica_t*erro_beta2
+ls_beta2
+
+
+
+
+
+
+
---
+++ ECONI-Sec05_02_IC_Variancia.R
@@ -0,0 +1,115 @@
+############################
+####### Métodos Exatos ######
+### www.metodosexatos.com ###
+#############################
+
+# Autor: André Santos | andre@metodosexatos.com.br
+# 05/08/2019
+
+# To cite R in publications use:
+# citation()
+
+###################################################################
+
+#--------------------- Diretórios e Arquivos ---------------------#
+
+# getwd() # Qual o diretório que o script está apontando
+# list.files() # Quais arquivos estão contidos no diretório
+# setwd("C:/Users/andre/OneDrive/Documentos/PROJETOS/Metodos Exatos/Cursos/Curso017_Econometria_I/Curso-ECON_Material_apoio/Datasets_Econ-I")
+
+# Leitura de uma base externa
+# leitura_csv2 <- read.csv2(file = "exemplo.csv")
+
+# Exportação de um arquivo no formato csv2 (formato brasileiro):
+# write.csv2(frame_carros, "exemplo.csv")
+
+# Leitura de arquivo externo usando pacotes
+
+# if (!require(package)) install.packages("xlsx")
+# library(xlsx)
+
+# read.xlsx("exemplo.xlsx", sheetName = "nome_planilha")
+
+# Salvar uma arquivo no formato xlsx
+# write.xlsx(nome_dataframe, "exemplo.xlsx")
+
+#---------------------------------------------------------------#
+
+#************* Intervalo de Confiança para os Coeficientes *************#
+
+# Variáveis
+X <- c(6, 7, 8, 9, 10, 11, 12, 13, 14,15, 16, 17, 18)
+Y <- c(4.4567, 5.77, 5.9787, 7.3317, 7.312, 6.5844, 7.8182, 7.851, 11.022, 10.674, 10.836, 13.615, 13.531)
+
+# Função de Regressão Amostral (FRA)
+
+FRA <- lm(Y~X) # modelo de regressão linear
+summary(FRA)   # resumo estatístico do modelo
+
+# Coeficientes
+
+install.packages("broom") # pacote para facilitar a extração de informações do FRA
+library("broom")
+
+info_cf <- tidy(FRA)      # tratamento do modelo com o pacote "broom"
+info_cf
+
+# Estimadores dos coeficientes
+beta1 <- info_cf$estimate[1]  # armazena apenas o valor do estimador do coeficiente beta 1 (intercepto)
+beta2 <- info_cf$estimate[2]  # armazena apenas o valor do estimador do coeficiente beta 2 (coeficiente angular)
+beta1                         # exibe o valor de beta 1
+beta2                         # exibe o valor de beta 2
+
+# Erro padrão dos estimadores
+erro_beta1 <- info_cf$std.error[1]  # armazena o termo de erro de beta 1
+erro_beta2 <- info_cf$std.error[2]  # armazena o termo de erro de beta 2
+erro_beta1                          # exibe o valor do termo de erro de beta 1
+erro_beta2                          # exibe o valor do termo de erro de beta 1
+
+# Estatistica t
+t <- t.test(X, Y, "two.sided", conf.level = 0.95)  # armazena o resultado do teste "t"
+t                                                  # exibe o resultado do teste "t"
+estatistica_t <- t$statistic[1]                    # armazena apenas o resultado da estatística t
+estatistica_t                                      # exibe o valor da estatística t
+
+# Intervalo de Confiança (IC)
+#- IC beta 1
+li_beta1 <- beta1 - estatistica_t*erro_beta1       # calcula o limite inferior de controle (LIC) para beta 1 e armazena na respectiva variável
+li_beta1                                           # exibe o LIC para beta 1
+ls_beta1 <- beta1 + estatistica_t*erro_beta1       # calcula o limite superior de controle (LSC) para beta 1 e armazena na respectiva variável
+ls_beta1                                           # exibe o LSC para beta 1
+
+#- IC beta 2
+li_beta2 <- beta2 - estatistica_t*erro_beta2       # calcula o limite inferior de controle (LIC) para beta 2 e armazena na respectiva variável
+li_beta2                                           # exibe o LIC para beta 2
+ls_beta2 <- beta2 + estatistica_t*erro_beta2       # calcula o limite superior de controle (LSC) para beta 2 e armazena na respectiva variável
+ls_beta2                                           # exibe o LSC para beta 2
+
+#- IC da variância
+alfa <- 0.05                                            # nível de significância
+gl <- df.residual(FRA)                                  # graus de liberdade
+qq_a2 <- qchisq(alfa/2, lower.tail = FALSE, df = gl)    # valor crítico X² para alfa/2
+qq_1a2 <- qchisq(alfa/2, lower.tail = TRUE, df = gl)    # valor crítico X² para 1-alfa/2
+
+EY <- beta1+beta2*X   # valores estimados de Y: E(Y|X)
+EY                    # exibe os valores estimados de Y
+
+Subt_Y <- Y-EY          # resíduos
+Subt_Y                  # exibe os resíduos
+sy2 <- Subt_Y^2         # resíduos elevados ao quadrado
+sy2                     # exibe os resíduos elevados ao quadrado
+soma_y2 <- sum(sy2)     # soma dos resíduos elevados ao quadrado
+soma_y2                 # exibe a soma dos resíduos elevados ao quadrado
+var_fra<- soma_y2/gl    # determina a variância do modelo
+var_fra                 # exibe a variância do modelo
+
+li_var <- gl*var_fra/qq_a2    # LIC para variância
+li_var                        # exibe o LIC para variância
+ls_var <- gl*var_fra/qq_1a2   # LSC para variância
+ls_var                        # exibe o LSC para variância
+
+
+
+
+
+
